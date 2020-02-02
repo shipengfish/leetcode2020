19e21413d24f0e8e3b5c30a04e8ff8d6c2fdea87
yushipeng <ysp@wozaijia.com>
2020-02-02 21:55:40 +0800

    hot100-617合并二叉树

---
+++ tree/src/main/java/MergeTwoBinaryTrees617.java
@@ -0,0 +1,32 @@
+/**
+ * @author shipengfish
+ * @version 1.0
+ * @date 2020/2/2
+ * @description https://leetcode-cn.com/problems/merge-two-binary-trees/
+ * @since 1.0
+ */
+public class MergeTwoBinaryTrees617 {
+
+    public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+        if (t1 == null) {
+            return t2;
+        }
+        if (t2 == null) {
+            return t1;
+        }
+        t1.val = t1.val + t2.val;
+        t1.left = mergeTrees(t1.left, t2.left);
+        t1.right = mergeTrees(t1.right, t2.right);
+        return t1;
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
